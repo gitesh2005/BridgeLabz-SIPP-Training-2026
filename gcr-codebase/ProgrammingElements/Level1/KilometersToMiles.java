8c696695a00976f78587c84e1f2cbe7375f0ce81
Gitesh Sorout <giteshsorout@gmail.com>
2026-06-09 20:19:54 +0530

    [Gitesh] Add: programming elements practice programs

---
+++ gcr-codebase/ProgrammingElements/Level1/KilometersToMiles.java
@@ -0,0 +1,11 @@
+package ProgrammingElements.Level1;
+
+public class KilometersToMiles {
+    public static void main(String[] args) {
+
+        double kilometer = 10.8;
+        double miles = kilometer / 1.6;
+
+        System.out.println("The distance " + kilometer + " km in miles is " + miles);
+    }
+}
