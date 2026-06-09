71b4fc3f247b27f07b72cbabe0f6fb4962bb5bb3
Gitesh Sorout <giteshsorout@gmail.com>
2026-06-09 20:23:53 +0530

    [Gitesh] Add: arithmetic and conversion programs

---
+++ gcr-codebase/ProgrammingElements/Level2/FeetToYardsAndMiles.java
@@ -0,0 +1,16 @@
+package ProgrammingElements.Level2;
+
+import java.util.Scanner;
+
+public class FeetToYardsAndMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double distanceInFeet = sc.nextDouble();
+
+        double distanceInYards = distanceInFeet / 3;
+        double distanceInMiles = distanceInYards / 1760;
+
+        System.out.println("The distance in yards is " + distanceInYards + " and miles is " + distanceInMiles);
+    }
+}
---
+++ gcr-codebase/ProgrammingElements/Level2/TriangleArea.java
@@ -0,0 +1,17 @@
+package ProgrammingElements.Level2;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        double areaInCm = 0.5 * base * height;
+        double areaInInches = areaInCm / (2.54 * 2.54);
+
+        System.out.println("The Area of the triangle in sq in is " + areaInInches + " and sq cm is " + areaInCm);
+    }
+}
